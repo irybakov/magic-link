25a00666067b8ec37cb4773888548bd3f8892d92
irybakov <irybakov@gmail.com>
2022-06-10 20:30:14 +0200

    PoC

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# magic-link
+# Intro
+This is the PoC of using Magic-link with Go-based API
+
+The `auth-magic` folder cotains login form and ufter login provides  the `token`.
+
+To login user must enter email on which the auth link will be sent.
+
+Click on the link provides the token on the client side (for example, browser)
+
+# Token
+
+To create token [Decentralized ID Token Specification](https://w3c-ccg.github.io/did-primer/#the-format-of-a-did) is used.
+Token is signed with `public key`
+
+# Access API
+To access API client uses token in the `Authorization header`
+```
+curl --location --request GET 'http://localhost:8090/hello' \
+--header 'Authorization: Bearer WyI... token ...jFiXCJ9Il0='
+```
+
+API (resource endpoints) should verify the `token` with `private key`
---
+++ main.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"strings"
+
+	"github.com/magiclabs/magic-admin-go"
+	"github.com/magiclabs/magic-admin-go/client"
+	"github.com/magiclabs/magic-admin-go/token"
+)
+
+const authBearer = "Bearer"
+
+var secret_key string
+
+var magicSDK *client.API
+
+func hello(w http.ResponseWriter, req *http.Request) {
+	// Check whether or not DIDT exists in HTTP Header Request
+	if !strings.HasPrefix(req.Header.Get("Authorization"), authBearer) {
+		fmt.Fprintf(w, "Bearer token is required")
+		return
+	}
+
+	// Retrieve DIDT token from HTTP Header Request
+	didToken := req.Header.Get("Authorization")[len(authBearer)+1:]
+	fmt.Printf("didToken: %s\n\n", didToken)
+
+	// Create a Token instance to interact with the DID token
+	tk, err := token.NewToken(didToken)
+	if err != nil {
+		fmt.Fprintf(w, "Malformed DID token error: %s", err.Error())
+		w.Write([]byte(err.Error()))
+		return
+	}
+
+	// Validate the Token instance before using it
+	if err := tk.Validate(); err != nil {
+		fmt.Fprintf(w, "DID token failed validation: %s", err.Error())
+		return
+	}
+
+	fmt.Fprintf(w, "Hello %s\n", tk)
+}
+
+func headers(w http.ResponseWriter, req *http.Request) {
+
+	for name, headers := range req.Header {
+		for _, h := range headers {
+			fmt.Fprintf(w, "%v: %v\n", name, h)
+		}
+	}
+}
+
+func main() {
+
+	secret_key = os.Getenv("MAGIC_SECRET")
+
+	magicSDK = client.New(secret_key, magic.NewDefaultClient())
+
+	http.HandleFunc("/hello", hello)
+	http.HandleFunc("/headers", headers)
+
+	http.ListenAndServe(":8090", nil)
+}
